207efcfb101d36fd33c7a77fca69177510b033a4
Carlos Angulo <41970288+carlosang2@users.noreply.github.com>
2019-02-16 15:22:56 -0500

    Test getAttendance()

--- test.php
+++ test.php
@@ -5,6 +5,7 @@ $zk->connect();
 $zk->disableDevice();
 
 $users = $zk->getUser();
+$attendace = $zk->getAttendance();
 ?>
 <table width="100%" border="1" cellspacing="0" cellpadding="0" style="border-collapse:collapse;">
 <thead>
@@ -33,6 +34,40 @@ foreach($users as $key=>$user)
     <td><?php echo $user[1];?></td>
     <td><?php echo $user[2];?></td>
     <td><?php echo $user[3];?></td>
+  </tr>
+
+<?php
+}
+?>
+
+</tbody>
+</table>
+<br /><br />
+<table width="100%" border="1" cellspacing="0" cellpadding="0" style="border-collapse:collapse;">
+<thead>
+  <tr>
+    <td width="25">No</td>
+    <td>UID</td>
+    <td>ID</td>
+    <td>State</td>
+    <td>Date/Time</td>
+  </tr>
+</thead>
+
+<tbody>
+<?php
+$no = 0;
+foreach($attendace as $key=>$at)
+{
+  $no++;
+?>
+
+  <tr>
+    <td align="right"><?php echo $no;?></td>
+    <td><?php echo $at[0];?></td>
+    <td><?php echo $at[1];?></td>
+    <td><?php echo $at[2];?></td>
+    <td><?php echo $at[3];?></td>
   </tr>
 
 <?php
